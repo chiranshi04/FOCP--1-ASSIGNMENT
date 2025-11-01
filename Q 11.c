6e701a45f2ecdbfd702543a6986809cd05fc01df
chiranshi04 <chiranshiyadav919@gmail.com>
2025-11-01 17:23:42 +0530

    Add program to classify even and odd numbers
    This program separates even and odd numbers from user input.

---
+++ Q 11.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() 
+{
+  int a[100], even[100], odd[100], n, e=0, o=0;
+   printf("Enter size: ");
+   scanf("%d", &n);
+
+    printf("Enter numbers: ");
+    for(int i=0;i<n;i++)
+      scanf("%d",&a[i]);
+
+    for(int i=0;i<n;i++) 
+    {
+        if(a[i]%2==0) even[e++]=a[i];
+        else odd[o++]=a[i];
+    }
+
+    printf("Even ");
+    for(int i=0;i<e;i++) printf("%d ",even[i]);
+
+    printf("\nOdd ");
+    for(int i=0;i<o;i++) printf("%d ",odd[i]);
+}
